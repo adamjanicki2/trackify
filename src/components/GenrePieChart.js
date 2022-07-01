e886f8ad8a659c895eb8342a27fec8540f57d16c
Adam Janicki <adamjanicki2@gmail.com>
2022-07-01 14:50:33 -0700

    final version maybe

--- src/components/GenrePieChart.js
+++ src/components/GenrePieChart.js
@@ -1,4 +1,51 @@
-import ReactApexChart from "react-apexcharts";
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+import "chart.js/auto";
+Chart.register(ArcElement);
+
+const COLORS = [
+  "#3182bd",
+  "#6baed6",
+  "#9ecae1",
+  "#c6dbef",
+  "#e6550d",
+  "#fd8d3c",
+  "#fdae6b",
+  "#fdd0a2",
+  "#31a354",
+  "#74c476",
+  "#a1d99b",
+  "#c7e9c0",
+  "#756bb1",
+  "#9e9ac8",
+  "#bcbddc",
+  "#dadaeb",
+  "#636363",
+  "#969696",
+  "#bdbdbd",
+  "#d9d9d9",
+  "#393b79",
+  "#5254a3",
+  "#6b6ecf",
+  "#9c9ede",
+  "#637939",
+  "#8ca252",
+  "#b5cf6b",
+  "#cedb9c",
+  "#8c6d31",
+  "#bd9e39",
+  "#e7ba52",
+  "#e7cb94",
+  "#843c39",
+  "#ad494a",
+  "#d6616b",
+  "#e7969c",
+  "#7b4173",
+  "#a55194",
+  "#ce6dbd",
+  "#de9ed6",
+];
 
 export default function GenrePieChart(props) {
   const { artists } = props;
@@ -21,82 +68,63 @@ export default function GenrePieChart(props) {
   const sortedEntries = Object.entries(genresToNumber).sort(
     (a, b) => b[1] - a[1]
   );
-
+  const data = {
+    labels: sortedEntries.map((entry) => entry[0]),
+    datasets: [
+      {
+        data: sortedEntries.map((entry) => entry[1]),
+        backgroundColor: COLORS,
+        hoverBackgroundColor: COLORS,
+        borderColor: "white",
+        radius: "100%",
+      },
+    ],
+  };
+  const totalCount = sortedEntries.reduce((prev, cur) => prev + cur[1], 0);
   return (
-    <div className="m-auto flex flex-column pa4" style={{ width: "60%" }}>
+    <div className="flex m-auto flex-column pa4 pie-width">
       <div className="top-display tc">Spotipie</div>
-      <ReactApexChart
+      <Pie
+        data={data}
         options={{
-          chart: {
-            type: "pie",
-          },
-          plotOptions: {
-            pie: {
-              startAngle: 0,
-              endAngle: 360,
+          plugins: {
+            legend: {
+              display: true,
+              position: "bottom",
+              labels: {
+                color: "#111111",
+                font: {
+                  size: 16,
+                  family: [
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    '"Helvetica Neue"',
+                    "sans-serif",
+                  ],
+                },
+              },
             },
-          },
-          dataLabels: {
-            enabled: true,
-          },
-          fill: {
-            type: "gradient",
-          },
-          labels: sortedEntries.map(([genre, number]) => genre),
-          colors: [
-            "#3182bd",
-            "#6baed6",
-            "#9ecae1",
-            "#c6dbef",
-            "#e6550d",
-            "#fd8d3c",
-            "#fdae6b",
-            "#fdd0a2",
-            "#31a354",
-            "#74c476",
-            "#a1d99b",
-            "#c7e9c0",
-            "#756bb1",
-            "#9e9ac8",
-            "#bcbddc",
-            "#dadaeb",
-            "#636363",
-            "#969696",
-            "#bdbdbd",
-            "#d9d9d9",
-            "#393b79",
-            "#5254a3",
-            "#6b6ecf",
-            "#9c9ede",
-            "#637939",
-            "#8ca252",
-            "#b5cf6b",
-            "#cedb9c",
-            "#8c6d31",
-            "#bd9e39",
-            "#e7ba52",
-            "#e7cb94",
-            "#843c39",
-            "#ad494a",
-            "#d6616b",
-            "#e7969c",
-            "#7b4173",
-            "#a55194",
-            "#ce6dbd",
-            "#de9ed6",
-          ],
-          legend: {
-            show: true,
-            formatter: function (val, opts) {
-              return sortedEntries[opts.seriesIndex][0];
-            },
-            tooltipHoverFormatter: function (val, opts) {
-              return sortedEntries[opts.seriesIndex][0];
+            tooltip: {
+              boxPadding: 4,
+              font: {
+                family: [
+                  "-apple-system",
+                  "BlinkMacSystemFont",
+                  '"Helvetica Neue"',
+                  "sans-serif",
+                ],
+              },
+              callbacks: {
+                label: function (context) {
+                  const percentage = ((context.raw * 100) / totalCount).toFixed(
+                    1
+                  );
+                  return `${context.label} (${percentage}%)`;
+                },
+              },
             },
           },
         }}
-        series={sortedEntries.map(([genre, amount]) => amount)}
-        type="pie"
       />
     </div>
   );
--- src/components/TopTracks.js
+++ src/components/TopTracks.js
@@ -1,27 +1,73 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { IconButton, Tooltip } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+const NUMBER_OF_TRACKS = 10;
+
+function getCopyText(tracks) {
+  let output = "My Top Tracks\n";
+  tracks.slice(0, NUMBER_OF_TRACKS).forEach((track, index) => {
+    const rank = index + 1;
+    const { name, artists, album } = track;
+    output += `#${rank} ${name}\n${artists[0].name} (${
+      album.release_date.split("-")[0]
+    })\n\n`;
+  });
+  return output;
+}
+
 export default function TopTracks(props) {
   const { tracks } = props;
-  const imgUrl = tracks[0].album.images[1].url;
+  const copyText = getCopyText(tracks);
   return (
     <div className="flex flex-column gradient-one pa4">
-      <div className="top-display tc">Top Tracks</div>
-      <div className="flex flex-column m-auto">
-        {tracks.slice(0, 10).map((track, index) => (
-          <div
-            className="flex flex-row flex-wrap items-center ma1 w-100"
+      <div className="top-display tc mb3">
+        Top Tracks
+        <CopyToClipboard text={copyText}>
+          <IconButton style={{ marginLeft: 8 }}>
+            <Tooltip title="Copy Top Tracks" style={{ color: "black" }} arrow>
+              <ContentCopyIcon style={{ color: "black" }} />
+            </Tooltip>
+          </IconButton>
+        </CopyToClipboard>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center m-auto">
+        {tracks.slice(0, NUMBER_OF_TRACKS).map((track, index) => (
+          <a
+            href={track.external_urls.spotify}
+            target="_blank"
             key={`toptrack${index}`}
+            rel="noreferrer"
+            className="no-underline black dim tracks-width"
           >
-            <div className="f2 fw4">#{index + 1}</div>
-            <img src={track.album.images[0].url} className="mh3 br2 img-size" />
-            <div className="flex flex-column">
-              <div className="f4 fw4">{track.name}</div>
-              <div className="f5 fw4">
-                {track.artists.map((artist) => artist.name).join(" & ")}
+            <div className="flex flex-column items-center ma1">
+              <div className="flex flex-row items-center">
+                <div className="f2 b ph3 bg-lightest-blue br3 gradient-one near-white ma2">
+                  {index + 1}
+                </div>
+                <img
+                  src={track.album.images[0].url}
+                  className="mh3 br2 img-size"
+                />
+                <div
+                  className="f2 b ph3 bg-lightest-blue br3 gradient-one near-white ma2"
+                  style={{ visibility: "hidden" }}
+                >
+                  {index + 1}
+                </div>
               </div>
-              <div className="f5 fw3">
-                {track.album.release_date.split("-")[0]}
+
+              <div className="flex flex-column tc">
+                <div className="f4 fw4">{track.name}</div>
+                <div className="f5 fw4">
+                  {track.artists.map((artist) => artist.name).join(" & ")}
+                </div>
+                <div className="f5 fw3">
+                  {track.album.release_date.split("-")[0]}
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
